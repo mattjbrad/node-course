dcfb63f45815b5c13094e0f7d7b63e9815e323c7
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-06 09:19:40 +0100

    functions

--- 02-functions/arrow.js
+++ 02-functions/arrow.js
@@ -13,19 +13,22 @@ var user = {
     name: 'Matt',
     sayHi: () =>{
         console.log(`Hi, ${this.name}`);
+        console.log(arguments);
     },
     sayHello(){
         console.log(`Hello, ${this.name}`);
+        console.log(arguments);
     },
     sayHola: function sayHola(){
         console.log(`Hola, ${this.name}`);
+        console.log(arguments);
     }
 };
-//Prints Undefined username
-user.sayHi();
+//Prints Undefined username as it doesn't bind the this keyword, also arguments are not bound to this options. Can use if don'e need this or arguments
+user.sayHi(1,2,3);
 
 //This one works as it works like a regular function, without needing explicit function. Only on methods on objects
-user.sayHello();
+user.sayHello(1,2,3);
 
 //this one is the old stle of defining it
-user.sayHola();
+user.sayHola(1,2,3);
