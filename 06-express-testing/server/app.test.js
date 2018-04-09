e6f28f9064ea882c164fee6cb551bb79d3b6d032
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-09 16:27:05 +0100

    Adding describe blocks to make them more readable

--- 06-express-testing/server/app.test.js
+++ 06-express-testing/server/app.test.js
@@ -2,24 +2,26 @@ const request = require('supertest');
 var app = require('./app').app;
 const expect = require('expect');
 
-it('should return this is the root', (done)=>{
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res)=>{
-            expect(res.body).toInclude({
-               error:"Page not found" 
-            });
-        })
-        .end(done);
-});
-
-it('should return the users', (done)=>{
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res)=>{
-            expect(res.body).toInclude({name:'matt', age:27});
-        })
-        .end(done);
-});
+describe('App Express', ()=>{
+    it('should return this is the root', (done)=>{
+        request(app)
+            .get('/')
+            .expect(404)
+            .expect((res)=>{
+                expect(res.body).toInclude({
+                   error:"Page not found" 
+                });
+            })
+            .end(done);
+    });
+    
+    it('should return the users', (done)=>{
+        request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body).toInclude({name:'matt', age:27});
+            })
+            .end(done);
+    });
+})
